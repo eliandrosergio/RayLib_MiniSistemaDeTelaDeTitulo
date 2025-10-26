e3aceae9754575687dc4b00ab6a283f07a7ee82a
eliandrosergio <eliandrosergio42@gmail.com>
2025-10-27 00:49:23 +0100

    opção de novo jogo atualizada

--- src/inc/hGame.hpp
+++ src/inc/hGame.hpp
@@ -4,12 +4,18 @@
 #include "hMain.hpp"
 
 #define Game_Width 400 // valor da largura da tela do jogo
-#define Game_Height 200 // valor da altura da tela do jogo
+#define Game_Height 250 // valor da altura da tela do jogo
 
 // struct principal do jogo
 struct	GameStruct {
 	MenuStruct	ms;
 	bool		isGameOver;
+	
+	// Dados do formulário
+	std::string nomeJogador;
+	std::string idadeJogador;
+	int			campoAtivo;  // 0 = nome, 1 = idade
+	bool		formularioCompleto;
 };
 
 // game.cpp
